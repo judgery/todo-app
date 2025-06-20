7221f78a158a429ffa7d18115f99677b1f9e8bdd
judge <seangregsy6@gmail.com>
2025-06-20 16:49:44 +0100

    Changed theme, added clock and fixed edit and complete button errors.

--- gui.py
+++ gui.py
@@ -1,18 +1,23 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("DarkBlue")
+
+clock = sg.Text("", key="clock")
 label = sg.Text("Enter a task")
 input_box = sg.InputText(tooltip="Enter Task", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todolist(),
                       key='todos',
-                      enable_events=True, size=[45, 10])
+                      enable_events=True, size=[35, 15])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('Planner App',
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
@@ -20,9 +25,8 @@ window = sg.Window('Planner App',
                    resizable=True)
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=500)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             tasks = functions.get_todolist()
@@ -31,21 +35,27 @@ while True:
             functions.write_tolist(tasks)
             window['todos'].update(values=tasks)
         case "Edit":
-            task_to_edit = values['todos'][0]
-            new_task = values['todo'] + "\n"
-            tasks = functions.get_todolist()
-            index = tasks.index(task_to_edit)
-            tasks[index] = new_task
-            functions.write_tolist(tasks)
-            window['todos'].update(values=tasks)
+            try:
+                task_to_edit = values['todos'][0]
+                new_task = values['todo'] + "\n"
+                tasks = functions.get_todolist()
+                index = tasks.index(task_to_edit)
+                tasks[index] = new_task
+                functions.write_tolist(tasks)
+                window['todos'].update(values=tasks)
+            except IndexError:
+                sg.popup("Please select an item", font=('Arial', 12))
 
         case "Complete":
-            task_to_complete = values['todos'][0]
-            tasks = functions.get_todolist()
-            tasks.remove(task_to_complete)
-            functions.write_tolist(tasks)
-            window['todos'].update(values=tasks)
-            window['todo'].update(value="")
+            try:
+                task_to_complete = values['todos'][0]
+                tasks = functions.get_todolist()
+                tasks.remove(task_to_complete)
+                functions.write_tolist(tasks)
+                window['todos'].update(values=tasks)
+                window['todo'].update(value="")
+            except IndexError:
+                sg.popup("Please select an item", font=('Arial', 12))
         case "Exit":
             break
         case 'todos':
