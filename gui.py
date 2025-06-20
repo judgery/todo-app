ac4ce9fa774c2b63f9f90f0a2003ba44e32b2631
judge <seangregsy6@gmail.com>
2025-06-20 16:13:14 +0100

    Implemented Add button 18/06/2025

--- gui.py
+++ gui.py
@@ -8,9 +8,14 @@ list_box = sg.Listbox(values=functions.get_todolist(),
                       key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('Planner App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Arial', 12),
                    resizable=True)
 
@@ -28,12 +33,21 @@ while True:
         case "Edit":
             task_to_edit = values['todos'][0]
             new_task = values['todo'] + "\n"
-
             tasks = functions.get_todolist()
             index = tasks.index(task_to_edit)
             tasks[index] = new_task
             functions.write_tolist(tasks)
             window['todos'].update(values=tasks)
+
+        case "Complete":
+            task_to_complete = values['todos'][0]
+            tasks = functions.get_todolist()
+            tasks.remove(task_to_complete)
+            functions.write_tolist(tasks)
+            window['todos'].update(values=tasks)
+            window['todo'].update(value="")
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
